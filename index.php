ab8cf445ab9792a7cdb8e3eda8d4cee60aa392a2
Seb-Prod <sebastien.drillaud@gmail.com>
2025-02-20 18:33:38 +0100

    ajout d'un historique de dévelopement

--- index.php
+++ index.php
@@ -6,6 +6,6 @@ require_once 'includes/header.php';
 ?>
 
 <!-- Début du contenu de la page -->
-
+<?php require_once 'infoDevelop.php'?>
 <!-- Fin du contenue de la page -->
 <?php require_once 'includes/footer.php' ?>
---
+++ infoDevelop.php
@@ -0,0 +1,13 @@
+<div class="card text-bg-info mb-3" style="max-width: 18rem;">
+    <div class="card-header">Seb-Prod</div>
+    <div class="card-body">
+        <h5 class="card-title">
+            <i class="fa-solid fa-clock"></i>
+            Historique
+        </h5>
+        <p>20/05/2025 - Ajout du formulaire pour ajouter une recette</p>
+        <p>19/05/2025 - Ajout du formulaire de connection et déconnection</p>
+        <p>18/05/2025 - Ajout du formulaire de création de compte</p>
+        <p>17/05/2025 - Initialisation du projet</p>
+    </div>
+</div>
